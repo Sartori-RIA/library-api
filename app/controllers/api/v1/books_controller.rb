c9efb2a896751fe4ae11cff87cd64ba067976d66
Lucas Sartori <faltou.criatividade0@gmail.com>
2024-08-09 06:44:02 -0300

    feat: added endpoints for books

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class Api::V1::BooksController < ApplicationController
+  load_and_authorize_resource
+
+  # GET /books
+  def index
+    render json: @books
+  end
+
+  # GET /books/1
+  def show
+    render json: @book
+  end
+
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /books/1
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1
+  def destroy
+    @book.destroy!
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :isbn, :total_copies)
+  end
+end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,13 @@ class Ability
 
   # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
   def initialize(user)
+    return if user.blank?
 
+    can :manage, User, id: user.id
+    if user.librarian?
+      can :manage, Book
+    else
+      can :index, Book
+    end
   end
 end
